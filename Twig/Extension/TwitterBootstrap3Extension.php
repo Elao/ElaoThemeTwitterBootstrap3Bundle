96d26c80e892f425273ef754a6d3d5eba1200e59
tom32i <thomas.jarrand@gmail.com>
2013-11-13 08:17:52 +0100

    Updated form horizontal

---
+++ Twig/Extension/TwitterBootstrap3Extension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Elao\ThemeBundle\TwitterBootstrap3Bundle\Twig\Extension;
+
+class TwitterBootstrap3Extension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'elao_theme_twitter_bootstrap_3_extension';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return array();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return array();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTests()
+    {
+        return array();
+    }
+}
