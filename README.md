1054b05444a6a05ea80ae5ee3d155579851bbf3b
Thomas JARRAND <thomas.jarrand@elao.com>
2013-11-13 16:37:43 +0100

    Added Tooltip and Popover

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ ElaoThemeTwitterBootstrap3Bundle
 ================================
 
 Elao Theme Twitter Bootstrap 3 Bundle
+
+Twig helpers
+------------
+
+### Tooltip & Popover data attributes renderers:
+
+    <a href="#" title="My title" {{ tooltip({placement: 'bottom'}) }}>
+        Hover me!
+    </a>
+
+The _tooltip_ fuction accepts arguments [as defined in Twitter Bootstrap 3](http://getbootstrap.com/javascript/#tooltips)
+
+    <div {{ popover({content: include('AcmeDemoBundle:Foo:bar.html.twig'), html: true}) }}>
+        Click me!
+    </div>
+
+The _popover_ fuction accepts arguments [as defined in Twitter Bootstrap 3](http://getbootstrap.com/javascript/#popovers)
+
+__Note:__ These functions only render html attributes, you need to do the Javascript part, ex:
+
+    <script>
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover();
+    </script>
--- Twig/Extension/TwitterBootstrap3Extension.php
+++ Twig/Extension/TwitterBootstrap3Extension.php
@@ -2,8 +2,28 @@
 
 namespace Elao\ThemeBundle\TwitterBootstrap3Bundle\Twig\Extension;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\Options;
+
 class TwitterBootstrap3Extension extends \Twig_Extension
 {
+    /**
+     * The Twig templating service
+     *
+     * @var Twig_Environment
+     */
+    private $templating;
+
+    /**
+     * Constructor
+     *
+     * @param Twig_Environment $templating
+     */
+    public function __construct(\Twig_Environment $templating)
+    {
+        $this->templating = $templating;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -17,7 +37,9 @@ class TwitterBootstrap3Extension extends \Twig_Extension
      */
     public function getFilters()
     {
-        return array();
+        return array(
+            new \Twig_SimpleFilter('bool_to_str', array($this, 'booleanToString')),
+        );
     }
 
     /**
@@ -25,7 +47,10 @@ class TwitterBootstrap3Extension extends \Twig_Extension
      */
     public function getFunctions()
     {
-        return array();
+        return array(
+            new \Twig_SimpleFunction('tooltip', array($this, 'tooltip'), array('is_safe' => array('html'))),
+            new \Twig_SimpleFunction('popover', array($this, 'popover'), array('is_safe' => array('html'))),
+        );
     }
 
     /**
@@ -34,5 +59,130 @@ class TwitterBootstrap3Extension extends \Twig_Extension
     public function getTests()
     {
         return array();
+    }
+
+    /**
+     * Tooltip
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function tooltip($parameters = array())
+    {
+        $resolver = new OptionsResolver();
+
+        $resolver->setOptional(
+            array(
+                'animation',
+                'html',
+                'placement',
+                'selector',
+                'title',
+                'trigger',
+                'delay',
+                'container',
+            )
+        );
+
+        $resolver->setAllowedTypes(
+            array(
+                'animation' => 'bool',
+                'html'      => 'bool',
+                'placement' => 'string',
+                'selector'  => array('string', 'bool'),
+                'title'     => 'string',
+                'container' => array('string', 'bool'),
+            )
+        );
+
+        $resolver->setAllowedValues(
+            array(
+                'placement' => array('top', 'bottom', 'left', 'right', 'auto'),
+                'trigger'   => array('click', 'hover', 'focus', 'manual'),
+            )
+        );
+
+        $attributes = array_merge(
+            $resolver->resolve($parameters),
+            array('toggle' => 'tooltip')
+        );
+
+        return $this->templating->render(
+            'ElaoThemeTwitterBootstrap3Bundle:Block:data_attributes.html.twig',
+            array(
+                'attributes' => $attributes,
+            )
+        );
+    }
+
+    /**
+     * Popover
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function popover($parameters = array())
+    {
+        $resolver = new OptionsResolver();
+
+        $resolver->setOptional(
+            array(
+                'animation',
+                'html',
+                'placement',
+                'selector',
+                'trigger',
+                'title',
+                'content',
+                'delay',
+                'container',
+            )
+        );
+
+        $resolver->setAllowedTypes(
+            array(
+                'animation' => 'bool',
+                'html'      => 'bool',
+                'placement' => 'string',
+                'selector'  => array('string', 'bool'),
+                'title'     => 'string',
+                'content'   => 'string',
+                'container' => array('string', 'bool'),
+            )
+        );
+
+        $resolver->setAllowedValues(
+            array(
+                'placement' => array('top', 'bottom', 'left', 'right', 'auto'),
+                'trigger'   => array('click', 'hover', 'focus', 'manual'),
+            )
+        );
+
+        $attributes = array_merge(
+            $resolver->resolve($parameters),
+            array('toggle' => 'popover')
+        );
+
+        return $this->templating->render(
+            'ElaoThemeTwitterBootstrap3Bundle:Block:data_attributes.html.twig',
+            array(
+                'attributes' => $attributes,
+            )
+        );
+    }
+
+    /**
+     * Give the string version of a boolean (for Twitter Boostrap 3 javascript config)
+     *
+     * @param boolean $value
+     * @return string
+     */
+    public function booleanToString($value = null)
+    {
+        if (is_bool($value)) {
+            $value = $value ? 'true' : 'false';
+        }
+
+        return $value;
     }
 }
